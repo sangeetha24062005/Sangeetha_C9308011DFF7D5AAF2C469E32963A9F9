18243c1a7dfa91019a672636a8ff7d34a92cc1c2
sangeetha24062005 <146157947+sangeetha24062005@users.noreply.github.com>
2023-09-29 13:55:21 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,37 @@
+# student list
+
+class student:
+
+  def __init__(self,name,roll_number,cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa 
+
+
+def sort_students(student_list):
+ #sort the list of students in descending order of CGPA
+  sorted_students = sorted(student_list, 
+                           
+key=lambda student: student.cgpa,
+
+reverse=True)
+ # syntax - lambda  arg:exp
+  return sorted_students
+
+
+#example usage:
+students = [
+    student("Hari", "A123", 7.8),
+    student("Srikanth","A124", 8.9),
+    student("saumya", "A125", 9.1),
+    student("Mahidar", "A126", 9.9),
+]
+
+sorted_students = sort_students(students)
+
+# print the sorted list of students
+for student in sorted_students:
+  print("Name: {}, Roll number: {}, CGPA: {}".format(student.name,
+                                                     
+student.roll_number,
+                                                     student.cgpa))
